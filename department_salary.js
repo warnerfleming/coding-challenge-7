8b39c1ac81f72a516ae806a88a9c114f2c2df316
warnerfleming <fleming14@usf.edu>
2024-10-04 10:09:07 -0400

    Initialize company structure with departments and employees

--- department_salary.js
+++ department_salary.js
@@ -1,1 +1,46 @@
-
+//Task 1 
+const company = {
+        departments: [
+        {
+            departmentName: 'Engineering',
+             employees: [  {
+                 name: 'Mark',
+                     salary: 100000,
+                   subordinates: [
+                      {
+                              name: 'Steve',
+                             salary: 70000,
+                            subordinates: [
+                            {
+                                     name: 'Lena',
+                                     salary: 50000,
+                                       subordinates: []
+                               }
+                             ]
+                          }
+         ]
+                  },
+               {
+              name: 'Greg',
+                   salary: 95000,
+             subordinates: []
+             }
+            ]
+        },
+           {
+              departmentName: 'Sales',
+          employees: [
+         {
+                   name: 'Boone',
+                     salary: 70000,
+                 subordinates: [
+                        {
+                         name: 'Pete',
+                                salary: 55000,
+                              subordinates: []
+                      }
+]}, {
+                   name: 'Grace',
+               salary: 80000,
+               subordinates: []
+           }]}]};
